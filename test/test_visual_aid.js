a63ebaa8ad4e9ffdc8137fd0b4ead14532007e79
pjaspers <pjaspers@air.local>
2020-12-13 21:35:35 +0100

    No, you *are* a module

--- test/test_visual_aid.js
+++ test/test_visual_aid.js
@@ -1,39 +1,43 @@
-var assert = require('assert'),
-    _ = require("lodash"),
-    aid = require("../visual-aid.js");
+const assert = require('assert');
+const _ = require("lodash");
+const { findUrlFor, available, availableExamples, regexToRuleThemAll } = require("../visual-aid.js");
 
-describe('when it hears', function () {
-  ["unexplainable", "interesting", "cue soffe", "cue raf", "cue nathan", "cue pivotal"].forEach(function(thing) {
-    it(thing + ", it returns a result", function () {
-      assert(aid(thing).length > 0);
-    });
-  });
-
-  it("cue hipster, it returns a moving picture of nate", function() {
-    assert(aid("cue hipster").length > 0);
-  });
-
-  it("cue pjaspers, it returns a pjaspers", function() {
-    assert(aid("cue pjaspers").length > 0);
-  });
-
-  it("cue HIPSTER, it doesn't care about the all caps and returns an image", function() {
-    assert(aid("cue HIPSTER").length > 0);
+describe('findUrlFor', function () {
+  available().forEach(({example, regexes, urls}) => {
+    it(`returns something for '${example}'`, () => (
+      assert(findUrlFor(example).length > 0)
+    ));
   });
 
   it("bobstinkt, it doesn't return a result", function () {
-    assert.strictEqual(aid("bobstinkt"), undefined);
-  });
-
-  it("replies when things are fine", function () {
-    assert(aid("Nah, this is fine").length > 0);
+    assert.strictEqual(findUrlFor("bobstinkt"), undefined);
   });
 
   it("doesn't replies when fine is used in a word", function () {
-    assert.strictEqual(aid("Nah, this is undefined"), undefined);
+    assert.strictEqual(findUrlFor("Nah, this is undefined"), undefined);
   });
 
   it("#also #thinks #hashtag #fine #is #fine", function () {
-    assert(aid("Everything is #fine").length > 0);
+    assert(findUrlFor("Everything is #fine").length > 0);
   });
 });
+
+describe("regexToRuleThemAll", () => {
+  available().forEach(({example}) => {
+    it(`matches '${example}'`, () => {
+      const regex = regexToRuleThemAll();
+      assert(regex.test(example));
+    });
+  });
+})
+
+describe("availableExamples", () => {
+
+  it("returns a bunch of examples", () => {
+    assert(availableExamples().length > 0)
+  })
+
+  it("contains at least fine", () => {
+    assert(availableExamples().includes("this is fine"));
+  })
+})
