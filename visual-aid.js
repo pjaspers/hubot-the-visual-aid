b4c735de7502b45ed9c2c96a2c119618b0bce9cc
pjaspers <piet@pjaspers.com>
2020-12-28 22:32:07 +0100

    Move wrapping word boundaries to main class

--- visual-aid.js
+++ visual-aid.js
@@ -16,6 +16,16 @@ const available = () => {
   return all.filter(e => e.urls.length > 0);
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+// If we don't see a regex, wrap it in \b's
+function wrapRegExp(string) {
+  if (/[.*+?^${}()|[\]\\]/.test(string)) {
+    return string;
+  } else {
+    return `(?:\\b|\\B)${string}\\b`;
+  }
+}
+
 // Goes over all the regexes defined in `data` and returns a random url defined in the object
 // whose regex matches. Otherwise returns nothing.
 module.exports = {
@@ -27,7 +37,7 @@ module.exports = {
   },
   findUrlFor: (input) => {
     const match = _.find(available(), thing => (
-      thing.regexes.some(regex => new RegExp(regex, "i").test(input))
+      thing.regexes.some(regex => new RegExp(wrapRegExp(regex), "i").test(input))
     ));
     if (match) {
       const url = _.sample(match.urls);
